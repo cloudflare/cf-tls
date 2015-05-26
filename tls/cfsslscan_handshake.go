dcb39c54e29263f0ba2c8d85e3c75ef1af51f8cb
Joshua Kroll <jkroll@jkroll.com>
2015-05-26 19:15:39 -0400

    Add support for scanning elliptic curve support in CFSSL.
    Specifically:
    -Add the IANA-named curves (and the curves for the CHACHA20_POLY1305 suites, which aren't IANA managed but are effectively standards).
    -Add support to the cfsslscan helper functions to move the handshake beyond ServerHello. Parse our the server's preferred curve ID and pass it back to CF-SSL for use in the scan.

--- tls/cfsslscan_handshake.go
+++ tls/cfsslscan_handshake.go
@@ -1,10 +1,8 @@
 package tls
 
-import "errors"
-
 // SayHello constructs a simple Client Hello to a server, parses its serverHelloMsg response
-// and returns the negotiated ciphersuite ID
-func (c *Conn) SayHello() (cipherID, version uint16, err error) {
+// and returns the negotiated ciphersuite ID, and, if an EC cipher suite, the curve ID
+func (c *Conn) SayHello() (cipherID, curveType uint16, curveID CurveID, version uint16, err error) {
 	hello := &clientHelloMsg{
 		vers:                c.config.maxVersion(),
 		compressionMethods:  []uint8{compressionNone},
@@ -22,9 +20,56 @@ func (c *Conn) SayHello() (cipherID, version uint16, err error) {
 	if err != nil {
 		return
 	}
-	cipherID, version = serverHello.cipherSuite, serverHello.vers
-	return
 
+	if CipherSuites[serverHello.cipherSuite].EllipticCurve {
+		// Prime the connection, if necessary, for key
+		// exchange messages by reading off the certificate
+		// message and, if necessary, the OCSP stapling
+		// message
+		var msg interface{}
+		msg, err = c.readHandshake()
+		if err != nil {
+			return
+		}
+		certMsg, ok := msg.(*certificateMsg)
+		if !ok || len(certMsg.certificates) == 0 {
+			err = unexpectedMessageError(certMsg, msg)
+			return
+		}
+		if serverHello.ocspStapling {
+			msg, err = c.readHandshake()
+			if err != nil {
+				return
+			}
+			certStatusMsg, ok := msg.(*certificateStatusMsg)
+			if !ok {
+				err = unexpectedMessageError(certStatusMsg, msg)
+				return
+			}
+		}
+
+		var skx *serverKeyExchangeMsg
+		skx, err = c.exchangeKeys()
+		if err != nil {
+			return
+		}
+		if skx.raw[0] != typeServerKeyExchange {
+			err = unexpectedMessageError(skx, msg)
+			return
+		}
+		if len(skx.key) < 4 {
+			err = unexpectedMessageError(skx, msg)
+			return
+		}
+		curveType = uint16(skx.key[0])
+		// If we have a named curve, report which one it is.
+		if curveType == 3 {
+			curveID = CurveID(skx.key[1])<<8 | CurveID(skx.key[2])
+		}
+	}
+	cipherID, version = serverHello.cipherSuite, serverHello.vers
+
+	return
 }
 
 // sayHello is the backend to SayHello that returns a full serverHelloMsg for processing.
@@ -36,8 +81,21 @@ func (c *Conn) sayHello(hello *clientHelloMsg) (serverHello *serverHelloMsg, err
 	}
 	serverHello, ok := msg.(*serverHelloMsg)
 	if !ok {
-		c.sendAlert(alertUnexpectedMessage)
-		return nil, errors.New("invalid ServerHello")
+		return nil, unexpectedMessageError(serverHello, msg)
+	}
+	return
+}
+
+// exchangeKeys continues the handshake to receive the serverKeyExchange message,
+// from which we can extract elliptic curve parameters
+func (c *Conn) exchangeKeys() (serverKeyExchange *serverKeyExchangeMsg, err error) {
+	msg, err := c.readHandshake()
+	if err != nil {
+		return
+	}
+	serverKeyExchange, ok := msg.(*serverKeyExchangeMsg)
+	if !ok {
+		return nil, unexpectedMessageError(serverKeyExchange, msg)
 	}
 	return
 }
